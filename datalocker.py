ad67573e362acfacbedbf21e45f45253115a1bc6
David Carlson <37191405+Figureix@users.noreply.github.com>
2019-10-25 20:22:03 -0600

    HealthCheck looking good. XbeeNode code DeepSleep working (remeber*settings on coord).

--- datalocker.py
+++ datalocker.py
@@ -17,15 +17,9 @@ lock = threading.Lock()  # Mutex for threads to grab when changing values here
 ProgramRunning = True
 
 # Initial States of system
-
 SystemEnabled = False
 NewSensorData = False
 
-avMoisture = 0
-avWind = 0
-avTemp = 0
-avRain = 0
-avHumid = 0
 
 def set_new():
     with lock:
@@ -46,46 +40,30 @@ def get_new():
 Thresholds = {
     "Moisture": [avMoisture, 0, 33], #TODO what is the max value of our sensor? How does that translate to moisture in soil
     "Wind": [avWind, 0, 5], #TODO The anemometer will return a small voltage range: What will be less-than-ideal wind?
-    "Temperature": [avTemp,0, 32], #TODO What unit/standard should we use here? Celcius?
-    "Rain": [avRain, 0, 5] #TODO say... mm (milimeters)?
+    "Temperature": [avTemp,0, 32] #TODO What unit/standard should we use here? Celcius?
 }
 
 
-# "Day": Active[bool], StartTime[int], EndTime[int]  -> (military time)
-# "Monday": [True, 0, 230] -> 0 == 12:00am, 230 == 2:30am, ... 2314 == 11:14pm
-# TODO reset these to default values
+# "Day": Active[bool], StartTime[int]  -> (military time)
+# "Monday": [True, 230] -> 230 == 2:30am
 TimerTriggering = {
-    "Sunday": [False, 0, 100],
-    "Monday": [False, 100, 230],
-    "Tuesday": [False, 245, 1000],
-    "Wednesday": [False, 1245, 1300],
-    "Thursday": [False, 1610, 1645],
-    "Friday": [False, 2345, 50],
-    "Saturday": [False, 1700, 1800]
+    "Sunday": [False, 300],
+    "Monday": [False, 300],
+    "Tuesday": [False, 300],
+    "Wednesday": [False, 300],
+    "Thursday": [False, 300],
+    "Friday": [False, 300],
+    "Saturday": [False, 300],
+    "Water Duration": 1200  # 1200 seconds == 20min
 }
 
-
-
-
+# Array of Dict.
 SensorStats = [None] * 4
-# TODO Ensure that the Main Coordinator is picked up in the SensorStats
 
+def SensorCount():
+    return len(SensorStats)
 
-
-
-# SensorStats = {}
-# SensorStats = {'xbee1': {'Moisture': 5, 
-#                             'Sunlight': 8, 
-#                             'Battery': 99, 
-#                             'Sector': 9, 
-#                             'Iteration': 33333}, 
-#                 'xbee2': {'Moisture': 5, 
-#                             'Sunlight': 8, 
-#                             'Battery': 99, 
-#                             'Sector': 9, 
-#                             'Iteration': 33333}, 
-#                 'xbee3': {'Moisture': 5, 
-#                             'Sunlight': 8, 
-#                             'Battery': 99, 
-#                             'Sector': 9, 
-#                             'Iteration': 33333}}
+HISTORY_LENGTH = 5
+NodeLastSeen_Light = [None] * HISTORY_LENGTH
+NodeLastSeen_Time = [None] * HISTORY_LENGTH
+NodeHealthStatus = [None] * HISTORY_LENGTH
